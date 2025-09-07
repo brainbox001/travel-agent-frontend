fd7f63818377f888377dff450c8939c220e1243a
Daniel Marcus <130494114+brainbox001@users.noreply.github.com>
2025-09-07 07:49:32 +0100

    Update api_url.ts

--- app/config/api_url.ts
+++ app/config/api_url.ts
@@ -1,2 +1,2 @@
-const API_URL = "https://brainbox001-travel-agent.hf.space/" //"http://localhost:8000"
+const API_URL = "https://brainbox001-travel-agent.hf.space" //"http://localhost:8000"
 export default API_URL;
